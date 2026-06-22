7dc77a2b5b7c3248480414315bf1dd8d5092ba4d
kyawthetkhaing255 <kyawthetkhaing255@gmail.com>
2026-06-22 19:24:19 +0630

    telegram webhook bot

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,22 @@
+# git init
+# git add .
+# git commit -m "telegram webhook bot"
+# git branch -M main
+# git remote add origin https://github.com/kyawthetkhaing255/telegram_bot
+# git push -u origin main
+
+
+# echo "# telegram-bot" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/kyawthetkhaing255/telegram-bot.git
+# git push -u origin main
+
 git init
 git add .
 git commit -m "telegram webhook bot"
+git remote add origin https://github.com/kyawthetkhaing255/telegram-bot.git
 git branch -M main
-git remote add origin https://github.com/kyawthetkhaing255/telegram_bot
 git push -u origin main
